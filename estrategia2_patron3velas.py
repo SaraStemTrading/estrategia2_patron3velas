68ecf97014a789834db60f01806cc90de795ce1e
Sara STEM | Trading Algorítmico <64358108+SaraStemTrading@users.noreply.github.com>
2022-06-19 16:55:34 +0200

    Update estrategia2_patron3velas.py

--- estrategia2_patron3velas.py
+++ estrategia2_patron3velas.py
@@ -52,101 +52,95 @@ for i in range(len(df)):
     elif df['Close'][i-1] > df['Open'][i-1] and df['Close'][i-2] > df['Open'][i-2] and df['Close'][i-3] > df['Open'][i-3] and df['Close'][i] < df['Open'][i] and ((df['High'][i-1] > df['UBB'][i-1]) and (df['High'][i-2] > df['UBB'][i-2]) and (df['High'][i-3] > df['UBB'][i-3])):
         df['entrada'][i] = -100
 
-# GESTIÓN DE STOPS
-df['pr_entrada'] = 0.0
-df['stop'] = 0.0
-df['posicion_c'] = 0
-df['posicion_v'] = 0
-df_ = df
-df_ = df_.reset_index()
+#GESTIÓN DE STOPS           
+df['pr_entrada']=0.0
+df['stop_compra']=0.0
+df['stop_venta']=0.0
+df_=df
+df_=df_.reset_index()
 
-# stop inicial
+#inicial
 for t in range(len(df)):
-    if df['entrada'][t] == 100:
-        df['pr_entrada'][t] = df['Close'][t]
+    if df['entrada'][t]==100:
+        df['pr_entrada'][t]=df['Close'][t]
         stop_a = df_.loc[t-4:t, 'Low'].min()
-        df['stop'][t] = stop_a
-    elif df['entrada'][t] == -100:
-        df['pr_entrada'][t] = df['Close'][t]
+        df['stop_compra'][t]=stop_a
+    elif df['entrada'][t]==-100:
+        df['pr_entrada'][t]=df['Close'][t]
         stop_b = df_.loc[t-4:t, 'High'].max()
-        df['stop'][t] = stop_b
+        df['stop_venta'][t]=stop_b
 
-df['stop'].replace(0, np.nan, inplace=True)
-df['stop'].fillna(method='ffill', inplace=True)
-df['stop'] = df['stop'].fillna(0)
-
-# stop final
+#final
 for x in range(len(df)):
-    if df['Close'][x] > df['UBB'][x]:
-        df['posicion_c'][x] = 1
-    if df['Low'][x] < df['MEDIA'][x]:
-        df['posicion_c'][x] = -1
-    if df['Close'][x] < df['LBB'][x]:
-        df['posicion_v'][x] = 2
-    if df['High'][x] > df['MEDIA'][x]:
-        df['posicion_v'][x] = -2
+    if df['Close'][x]>df['UBB'][x]:
+        df['stop_compra'][x]=df['MEDIA'][x]
+    if df['Close'][x]<df['LBB'][x]:
+        df['stop_venta'][x]=df['MEDIA'][x]
 
-cols = ['posicion_c', 'posicion_v']
-df[cols].replace(0, np.nan, inplace=True)
-df[cols].fillna(method='ffill', inplace=True)
+df['stop_compra'].replace(0, np.nan, inplace=True)
+df['stop_compra'].fillna(method='ffill', inplace=True)
+df['stop_compra'] = df['stop_compra'].fillna(0)
+df['stop_venta'].replace(0, np.nan, inplace=True)
+df['stop_venta'].fillna(method='ffill', inplace=True)
+df['stop_venta'] = df['stop_venta'].fillna(0)
 
-# VOLUMEN A INVERTIR
-df['contratos'] = 0
-df = df.dropna()
+#VOLUMEN A INVERTIR
+df['contratos_compra']=0
+df=df.dropna()
 for x in range(len(df)):
-    if activo[-1] == 'X':  # en divisas
-        if df['pr_entrada'][x] > 0:
-            df['contratos'][x] = math.ceil(
-                abs((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop'][x])*0.00001))
+    if activo[-1]=='X': #en divisas
+        if df['pr_entrada'][x]>0 and df['entrada'][x]==100:
+            df['contratos_compra'][x]=math.ceil((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop_compra'][x])*0.0001)
     else:
-        if df['pr_entrada'][x] > 0:
-            df['contratos'][x] = math.ceil(
-                abs((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop'][x])))
+        if df['pr_entrada'][x]>0 and df['entrada'][x]==100:
+            df['contratos_compra'][x]=math.ceil((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop_compra'][x]))
+            
+df['contratos_venta']=0
+df=df.dropna()
+for x in range(len(df)):
+    if activo[-1]=='X': #en divisas
+        if df['pr_entrada'][x]>0 and df['entrada'][x]==-100:
+            df['contratos_venta'][x]=math.ceil(abs((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop_venta'][x])*0.0001)) 
+    else:
+        if df['pr_entrada'][x]>0 and df['entrada'][x]==-100:
+            df['contratos_venta'][x]=math.ceil(abs((riesgo_op*capital)/(df['pr_entrada'][x]-df['stop_venta'][x])))
 
-# DEFINIMOS EL BACKTEST
+        
+#DEFINIMOS EL BACKTEST
 class estrategia2(SignalStrategy):
 
     def init(self):
         super().init()
-
+        
     def next(self):
         super().next()
         pr_entrada = self.data.pr_entrada
-        entrada = self.data.entrada
-        contratos = self.data.contratos
-        media = self.data.MEDIA
-        stop = self.data.stop
-        if entrada == 100 and pr_entrada > 0:
-            alcista = contratos[np.argwhere(
-                (entrada == 100) & (pr_entrada > 0))[-1]]
+        entrada=self.data.entrada
+        contratos_compra=self.data.contratos_compra
+        contratos_venta=self.data.contratos_venta
+        stop_compra=self.data.stop_compra
+        stop_venta=self.data.stop_venta
+        if entrada==100 and pr_entrada>0:
+            alcista=contratos_compra[np.argwhere((entrada==100) & (pr_entrada>0))[-1]]
             self.buy(size=alcista[0])
-        if entrada == -100 and pr_entrada > 0:
-            bajista = contratos[np.argwhere(
-                (entrada == -100) & (pr_entrada > 0))[-1]]
-            self.sell(size=bajista[0])
+        if entrada==-100 and pr_entrada>0:
+            bajista=contratos_venta[np.argwhere((entrada==-100) & (pr_entrada>0))[-1]]
+            self.sell(size=bajista[0])            
         for trade in self.trades:
             if trade.is_long:
-                trade.sl = stop
-                if self.data.posicion_c == 1:
-                    trade.sl = media
+                trade.sl=stop_compra
             if trade.is_short:
-                trade.sl = stop
-                if self.data.posicion_v == 2:
-                    trade.sl = media
+                trade.sl=stop_venta
 
-
-btest = Backtest(df, estrategia2, cash=capital, commission=comision,
-                 exclusive_orders=False, hedging=True, trade_on_close=False, margin=margen)
-stats = btest.run()
+btest = Backtest(df, estrategia2, cash=capital, commission=comision, exclusive_orders=False, hedging=True, trade_on_close=True, margin=margen)
+stats=btest.run()
 btest.plot(open_browser=False)
 
-# Pintamos los resultados
 resultados={}
 rentabilidad=round(stats[6],2)
 n_operaciones=stats[17]
 ratio_aciertos=round(stats[18],2)
 drawdown=round(stats[13],2)
-resultados={'Rentabilidad (%)':rentabilidad, 'Número de operaciones':n_operaciones, 'Ratio de aciertos (%)':ratio_aciertos, 'Máximo Dradown (%)':drawdown}
+resultados={'Rentabilidad':rentabilidad, 'Número de operaciones':n_operaciones, 'Ratio de aciertos (%)':ratio_aciertos, 'Máximo Dradown (%)':drawdown}
 df_resultados = pd.DataFrame(resultados,index=['Estrategia Nº2'])
 print(df_resultados)
-
